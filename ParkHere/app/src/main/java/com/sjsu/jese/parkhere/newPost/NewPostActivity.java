351fd615a40c2c79b3d4fc94afa0a3c7e1184900
elvinh <eltonv93@gmail.com>
2017-10-31 20:28:03 -0700

    Created files for creating a new parking space post

---
+++ ParkHere/app/src/main/java/com/sjsu/jese/parkhere/newPost/NewPostActivity.java
@@ -0,0 +1,15 @@
+package com.sjsu.jese.parkhere.newPost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sjsu.jese.parkhere.R;
+
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
