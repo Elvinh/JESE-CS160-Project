d9db0f35bbc1af7bb10feef3e48c5b330025ab24
elvinh <eltonv93@gmail.com>
2017-11-01 22:25:55 -0700

    Changed color of bg to white
    also removed a button and fixed error

--- ParkHere/app/src/main/java/com/sjsu/jese/parkhere/MainActivity.java
+++ ParkHere/app/src/main/java/com/sjsu/jese/parkhere/MainActivity.java
@@ -28,46 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
 
-    TextView mCustomerTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCustomerTextView = (TextView) findViewById(R.id.customerTextView);
-        final Button newCustomerButton = (Button) findViewById(R.id.newAddressButton);
-
-        // add new customer to Firebase database. Input is currently hardcoded in
-        newCustomerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Address address = new Address("1512 Sun Ln", "San Jose", "CA",95132, "United States");
-                Customer customer = new Customer("asas", 2648907, address);
-                myCustomerRef.child("12341").setValue(customer);
-            }
-        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // logic to display current logged in user info
-        if (user.getUid() != null) {
-            myCustomerRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Customer customer = dataSnapshot.getValue(Customer.class);
-                    mCustomerTextView.setText("Logged in as: " + customer.getName());
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-        }
     }
 
     // When button id = browsePostBtn clicked takes you to BrowsePostActivity
