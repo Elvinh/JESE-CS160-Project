9c5958a2e84c136292963cb6b626ff989f108417
jgaines2013 <jgaines2013@gmail.com>
2017-11-05 21:49:03 -0800

    adding window for booking activity

---
+++ ParkHere/app/src/main/java/com/sjsu/jese/parkhere/Book/BookActivity.java
@@ -0,0 +1,15 @@
+package com.sjsu.jese.parkhere.Book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sjsu.jese.parkhere.R;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
