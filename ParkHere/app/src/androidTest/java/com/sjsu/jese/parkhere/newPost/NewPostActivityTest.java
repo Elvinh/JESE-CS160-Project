e2e9e3147b98ef2c98841f0f54ee91a85aca37d4
elvinh <eltonv93@gmail.com>
2017-11-09 23:34:06 -0800

    more testssss

--- ParkHere/app/src/androidTest/java/com/sjsu/jese/parkhere/newPost/NewPostActivityTest.java
+++ ParkHere/app/src/androidTest/java/com/sjsu/jese/parkhere/newPost/NewPostActivityTest.java
@@ -1,5 +1,6 @@
 package com.sjsu.jese.parkhere.newPost;
 
+import android.app.DatePickerDialog;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
@@ -28,11 +29,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.sjsu.jese.parkhere.login.LoginActivityTest.hasTextInputLayoutHintError;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
+import android.support.test.espresso.contrib.PickerActions;
 
 /**
  * Created by Elton on 11/9/2017.
@@ -81,15 +85,71 @@ public class NewPostActivityTest {
     }
 
    @Test
-    public void checkCalanderOption() {
+    public void checkZipCodeError() {
+       onView(withId(R.id.nextBtn)).perform(click());
+       onView(withId(R.id.nextBtn)).perform(click());
 
 
-        onView(withId(R.id.input_layout_start_date)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
-                .perform(PickerActions.setDate(2017, 1, 1));
+       onView(withId(R.id.zipField))
+               .perform(typeText("test"), closeSoftKeyboard());
+
+
+       onView(withId(R.id.nextBtn)).perform(click());
+
+       onView(withId(R.id.input_layout_zip))
+               .check(matches(hasTextInputLayoutHintError("Not a valid zip code.")));
+   }
+
+    @Test
+    public void checkZipCodeNoError() {
         onView(withId(R.id.nextBtn)).perform(click());
-        onView(withId(R.id.startDateField)).check(matches(isEditTextValueEqualTo("1/01/17")));
-        //assertEquals(mActivityRule.getActivity().getNewPost().getDateAvailable(), "");
+        onView(withId(R.id.nextBtn)).perform(click());
 
+
+        onView(withId(R.id.zipField))
+                .perform(typeText("95132"), closeSoftKeyboard());
+
+
+        onView(withId(R.id.input_layout_zip))
+                .check(matches(not(hasTextInputLayoutHintError("Not a valid zip code."))));
+
+        onView(withId(R.id.nextBtn)).perform(click());
+    }
+
+    @Test
+    public void checkAddressSetInPost() {
+        onView(withId(R.id.nextBtn)).perform(click());
+        onView(withId(R.id.nextBtn)).perform(click());
+
+        onView(withId(R.id.stateField))
+                .perform(typeText("CA"), closeSoftKeyboard());
+        onView(withId(R.id.cityField))
+                .perform(typeText("Milpitas"), closeSoftKeyboard());
+        onView(withId(R.id.countryField))
+                .perform(typeText("United States"), closeSoftKeyboard());
+        onView(withId(R.id.streetField))
+                .perform(typeText("2196 Mesa Verde Dr."), closeSoftKeyboard());
+        onView(withId(R.id.zipField))
+                .perform(typeText("95132"), closeSoftKeyboard());
+
+
+
+        onView(withId(R.id.nextBtn)).perform(click());
+
+        int zip = 95132;
+        assertEquals(mActivityRule.getActivity().getNewPost().getAddress().getZipCode(), zip);
+        assertEquals(mActivityRule.getActivity().getNewPost().getAddress().getState(), "CA");
+        assertEquals(mActivityRule.getActivity().getNewPost().getAddress().getCity(), "Milpitas");
+        assertEquals(mActivityRule.getActivity().getNewPost().getAddress().getCountry(), "United States");
+        assertEquals(mActivityRule.getActivity().getNewPost().getAddress().getStreetAddress(), "2196 Mesa Verde Dr.");
+    }
+
+    @Test
+    public void checkDatesSetInPost() {
+        onView(withId(R.id.nextBtn)).perform(click());
+        onView(withId(R.id.nextBtn)).perform(click());
+
+        assertNotNull(mActivityRule.getActivity().getNewPost().getDateAvailable());
+        assertNotNull(mActivityRule.getActivity().getNewPost().getDateEnd());
     }
 }
