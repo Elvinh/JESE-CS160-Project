78e5ac6881f35e14f003f6516ae3dc76c02ff604
elvinh <eltonv93@gmail.com>
2017-12-02 20:11:04 -0800

    Change to show image that user uploaded

--- ParkHere/app/src/main/java/com/sjsu/jese/parkhere/postDetails/PostDetailActivity.java
+++ ParkHere/app/src/main/java/com/sjsu/jese/parkhere/postDetails/PostDetailActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.sjsu.jese.parkhere.Book.BookActivity;
 import com.sjsu.jese.parkhere.R;
 import com.sjsu.jese.parkhere.model.Address;
@@ -41,6 +46,7 @@ public class PostDetailActivity extends AppCompatActivity {
         final TextView mStartDate = (TextView) findViewById(R.id.startDateText);
         final TextView mEndDate = (TextView) findViewById(R.id.endDateText);
         final Button mBookBt=(Button) findViewById(R.id.book_button);
+        final ImageView mPostImage = (ImageView) findViewById(R.id.post_detail_image);
 
         mPost.addValueEventListener(new ValueEventListener() {
             @Override
@@ -61,6 +67,14 @@ public class PostDetailActivity extends AppCompatActivity {
             }
         });
 
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference imageRef = storageRef.child("images/" + postId + "/post_image");
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(imageRef)
+                .into(mPostImage);
+        
         mBookBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
