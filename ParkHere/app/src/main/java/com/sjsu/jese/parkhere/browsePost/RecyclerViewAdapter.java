95256f3282454b0d3a8b5322801ea278515f488b
Elton <eltonv93@gmail.com>
2017-12-02 19:28:28 -0800

    support for uploading images for posts

--- ParkHere/app/src/main/java/com/sjsu/jese/parkhere/browsePost/RecyclerViewAdapter.java
+++ ParkHere/app/src/main/java/com/sjsu/jese/parkhere/browsePost/RecyclerViewAdapter.java
@@ -3,13 +3,17 @@ package com.sjsu.jese.parkhere.browsePost;
 
 import android.content.Context;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.sjsu.jese.parkhere.R;
 import com.sjsu.jese.parkhere.model.Post;
 import com.sjsu.jese.parkhere.postDetails.PostDetailActivity;
@@ -40,6 +44,10 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
         holder.name.setText(posts.get(position).getTitle());
         holder.price.setText("$" + String.valueOf((int)posts.get(position).getDailyRate()) + " per hour");
         holder.currPost = posts.get(position);
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(storageReference)
+                .into(holder.postImage);
     }
 
     @Override
@@ -53,6 +61,7 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
         private TextView size;
         private Post currPost;
         private View view;
+        private ImageView postImage;
 
         ViewHolder (View itemView) {
             super(itemView);
@@ -60,6 +69,7 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
             name = (TextView) itemView.findViewById(R.id.item_title);
             price = (TextView) itemView.findViewById(R.id.item_price);
             size = (TextView) itemView.findViewById(R.id.item_size);
+            postImage = (ImageView) itemView.findViewById(R.id.imageView3);
 
             view.setOnClickListener(new View.OnClickListener() {
                @Override
